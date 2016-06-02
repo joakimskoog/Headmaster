c83a6f00ad1a37a68628e5372fe5b54488cd87c5
Joakim Skog <joakims@centiro.se>
2016-06-02 19:49:33 +0200

    RequestMessageExtensions

---
+++ Headmaster.Core/HttpRequestMessageExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+
+namespace Headmaster.Core
+{
+    public static class HttpRequestMessageExtensions
+    {
+        public static string GetApiVersion(this HttpRequestMessage request, string mediaType, string mediaTypeParameter)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request));
+            if (string.IsNullOrEmpty(mediaType)) throw new ArgumentNullException(nameof(mediaType));
+            if (string.IsNullOrEmpty(mediaTypeParameter)) throw new ArgumentNullException(nameof(mediaTypeParameter));
+
+            var acceptHeader = request.Headers.Accept;
+
+            foreach (var mime in acceptHeader.OrderByDescending(x => x.Quality))
+            {
+                if (string.Equals(mime.MediaType, mediaType, StringComparison.OrdinalIgnoreCase))
+                {
+                    var version = mime.Parameters.FirstOrDefault(x => string.Equals(x.Name, mediaTypeParameter, StringComparison.OrdinalIgnoreCase));
+
+                    return version?.Value ?? string.Empty;
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
